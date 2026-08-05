04f5cbca06311c2a471b9541499094d1874d6e6f
Egehan Giral <111067206+alllllllllllllllllllllllllllllllllll@users.noreply.github.com>
2026-08-05 13:24:01 +0300

    Create data.js

---
+++ out/html/data.js
@@ -0,0 +1,140 @@
+const tooltipList = [{
+    searchString: "CHP",
+    explanationText: "<img src=img/chp5_20251125_200815_0000.svg> Republican People's Party, our party."
+},
+{
+    searchString: "AP",
+    explanationText: "<img src=img/AP.jpg> Justice Party, our primary adversary."
+},
+{
+    searchString: "TIP",
+    explanationText: "<img src=img/TİP_1970'ler.png> Workers Party of Turkey"
+},
+{
+    searchString: "TİP",
+    explanationText: "<img src=img/TİP_1970'ler.png> Workers Party of Turkey"
+},                    
+{
+    searchString: "DP",
+    explanationText: "<img src=img/DP.png> Democratic Party"
+},
+{
+    searchString: "THKO",
+    explanationText: "<img src=img/thko.jpeg> A Marxist-Leninist terrorist group inspired by the Cuban revolution and Guevarism"
+},                     
+{
+    searchString: "THKP-C",
+    explanationText: "<img src=img/THKP-C.png> A Marxist-Leninist terrorist group"
+},
+{
+    searchString: "TKP/ML",
+    explanationText: "<img src=img/TKPML.svg.png> A Marxist-Leninist-Maoist terrorist group"
+},
+{
+    searchString: "TİT",
+    explanationText: "An Ultranationalist terrorist organization that the public knows almost nothing about."
+},                     
+{
+    searchString: "CGP",
+    explanationText: "<img src=img/cgplogo.png> Republican Trust Party"
+},
+{
+    searchString: "MSP",
+    explanationText: "<img src=img/MSP.png> National Salvation Party"
+},
+{
+    searchString: "MNP",
+    explanationText: "<img src=img/mnp.png> National Order Party. The predecessor to MSP."
+},                     
+{
+    searchString: "MHP",
+    explanationText: "<img src=img/MHP.png> Nationalist Movement Party"
+},
+{
+    searchString: "CKMP",
+    explanationText: "<img src=img/CKMP.png> Republican Peasant Nation Party. The Nationalist predecessor to MHP."
+},                    
+{
+    searchString: "Blue Mosque",
+    explanationText: "<img src=img/blue_mosque.jpeg> The Blue Mosque(Sultanahmet Camii) is one of the most iconic monuments of Ottoman architecture, built between 1606-1617. Its famous for being the first Turkish Mosque with 6 minarets."
+},                     
+];
+
+
+const colourList = [{
+        word: "CHP",
+        style: "color: #E3000F; font-weight: bold;"
+},
+{
+        word: "AP",
+        style: "color: #750a6f; font-weight: bold;"
+},
+{
+        word: "CGP",
+        style: "color: #5a5f63; font-weight: bold;"
+},
+{
+        word: "TIP",
+        style: "color: #8B0000; font-weight: bold;"
+},
+{
+        word: "TİP",
+        style: "color: #8B0000; font-weight: bold;"
+},                    
+{
+        word: "Raiders",
+        style: "color: #367824; font-weight: bold;"
+},                                       
+{
+        word: "THKP-C",
+        style: "color: #f6D248; font-weight: bold;"
+},
+{
+        word: "TKP/ML",
+        style: "color: #990000; font-weight: bold;"
+},
+{
+        word: "THKO",
+        style: "color: #c4772e; font-weight: bold;"
+},                  
+{
+        word: "DP",
+        style: "color: #4a4d04; font-weight: bold;"
+},
+{
+        word: "MSP",
+        style: "color: #045421; font-weight: bold;"
+},
+{
+        word: "MNP",
+        style: "color: #045421; font-weight: bold;"
+},                    
+{
+        word: "MHP",
+        style: "color: #954B00; font-weight: bold;"
+},       
+{
+        word: "CKMP",
+        style: "color: #b55d04; font-weight: bold;"
+},                    
+{
+        word: "SBP",
+        style: "color: #9E5747; font-weight: bold;"
+},
+{
+        word: "DEV-YOL",
+        style: "color: #d94104; font-weight: bold;"
+},
+{
+        word: "Grey Wolves",
+        style: "color: #190c91; font-weight: bold;"
+},
+{
+        word: "TİT",
+        style: "color: #181B18; font-weight: bold;"
+},                    
+{
+        word: "Blue Mosque",
+        style: "color: #42b6f5; font-weight: bold;"
+},                                        
+];
